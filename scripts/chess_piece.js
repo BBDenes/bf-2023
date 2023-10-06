4502b1ff4fda42d5792902e3880203acb2c0b5f0
BBDenes <csennel302@gmail.com>
2023-10-06 14:28:16 +0200

    pieces start placing

--- scripts/chess_piece.js
+++ scripts/chess_piece.js
@@ -1,3 +1,5 @@
+const layout = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']
+
 
 class Piece{
     constructor(team, type, x, y, id){
@@ -10,6 +12,20 @@ class Piece{
 
     draw(){
         const tile = document.querySelector(String(this.id));
-        
+        let source = `${this.team}/chess-${this.type}-regular.svg`;
+        let newElement = document.createElement("img");
+        newElement.src = source;
+        newElement.height = "3rem";
+        tile.append(newElement);
     }
 };
+
+const piece_table = [];
+
+function drawPieces() {
+    piece_table.push(new Array);
+    for (let i = 0; i < 8; i++) {
+        piece_table[0][i] = new Piece('black', layout[i], 0, i, i);
+    }
+    
+}
